02c0a8944032486bf04b00a9c17a530bb73044da
Hakon Hanesand <hakon@hanesand.no>
2013-01-12 11:37:29 -0600

    Initial commit

---
+++ com/github/BigRick/BigTree/core/BigTree.java
@@ -0,0 +1,31 @@
+package com.github.BigRick.BigTree.core;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "BigTree", name = "Big Tree Generation Mod", version = "0.0.1")
+public class BigTree {
+	@Instance("BigTree")
+	public static BigTree instance;
+
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event) {
+
+	}
+
+	@Init
+	void load(FMLInitializationEvent event) {
+
+	}
+
+	@PostInit
+	public void postInit(FMLPostInitializationEvent event) {
+
+	}
+}
