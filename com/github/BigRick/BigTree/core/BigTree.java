718ae7aba524efba60698ef590e71d9af61efb43
Hakon Hanesand <hakon@hanesand.no>
2013-01-13 12:56:45 -0600

    Added cofig

--- com/github/BigRick/BigTree/core/BigTree.java
+++ com/github/BigRick/BigTree/core/BigTree.java
@@ -1,5 +1,7 @@
 package com.github.BigRick.BigTree.core;
 
+import com.github.BigRick.config.ConfigurationHandler;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
@@ -10,22 +12,29 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "BigTree", name = "Big Tree Generation Mod", version = "0.0.1")
-public class BigTree {
+public class BigTree
+{
 	@Instance("BigTree")
 	public static BigTree instance;
 
 	@PreInit
-	public void preInit(FMLPreInitializationEvent event) {
-
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		/*
+		 * Initialize the configuration
+		 */
+		ConfigurationHandler.initConfig(event.getSuggestedConfigurationFile());
 	}
 
 	@Init
-	void load(FMLInitializationEvent event) {
-
+	void load(FMLInitializationEvent event)
+	{
+		event.
 	}
 
 	@PostInit
-	public void postInit(FMLPostInitializationEvent event) {
+	public void postInit(FMLPostInitializationEvent event)
+	{
 
 	}
 }
---
+++ com/github/BigRick/config/ConfigurationHandler.java
@@ -0,0 +1,29 @@
+package com.github.BigRick.config;
+
+import java.io.File;
+import java.util.logging.Level;
+
+import net.minecraftforge.common.Configuration;
+import cpw.mods.fml.common.FMLLog;
+
+public class ConfigurationHandler
+{
+	public static Configuration config;
+
+	public static void initConfig(File configFile)
+	{
+		config = new Configuration(configFile);
+		
+		try
+		{
+			config.load();
+			
+			
+			
+			config.save();
+		} catch (Exception e)
+		{
+			FMLLog.log(Level.SEVERE, e, "Oil Craft" + " has had a problem loading its configuration");
+		}
+	}
+}
